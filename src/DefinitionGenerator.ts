a4a261ecd0226a7609e1bb7016ee3a77138cafdc
Raphael Silva Do Nascimento <raphael.nascimento@grupoboticario.com.br>
2024-08-13 11:49:21 -0300

    update query and parameter generator

--- src/DefinitionGenerator.ts
+++ src/DefinitionGenerator.ts
@@ -144,11 +144,11 @@ export class DefinitionGenerator {
       operationObj.deprecated = true;
     }
 
-    if (operationObj.requestBody) {
+    if (documentationConfig.requestBody) {
       operationObj.requestBody = this.getRequestBodiesFromConfig(documentationConfig);
     }
 
-    if (operationObj.parameters) {
+    if (documentationConfig.queryParams) {
       operationObj.parameters = this.getParametersFromConfig(documentationConfig);
     }
 
